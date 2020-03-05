03896383a12260eb019297b0ec8a5ea4ccfa8ce7
bonifacyi <bonifacyi1102@gmail.com>
2020-03-05 17:17:15 +0300

    DB changed to Postgres. Added new posts

--- blogtest/blogtest/settings.py
+++ blogtest/blogtest/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'taggit',
     'django.contrib.sites',
     'django.contrib.sitemaps',
+    'django.contrib.postgres',
 ]
 
 MIDDLEWARE = [
@@ -79,11 +80,18 @@ WSGI_APPLICATION = 'blogtest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blog',
+        'USER': 'blog',
+        'PASSWORD': 'kt315a',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    },
 }
-
+"""'default': {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+},"""
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators

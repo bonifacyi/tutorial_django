0dbbd61d902970ff2cb8b196299ae6ca6bcc6719
bonifacyi <bonifacyi1102@gmail.com>
2020-03-20 10:51:51 +0300

    Add user registration

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register'),
 ]
 
 """urlpatterns = [
